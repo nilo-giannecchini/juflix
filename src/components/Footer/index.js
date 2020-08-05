85468e1725aecfb67b97e545c13163852a522c9a
Nilo Giannecchini <nilo.giannecchini@gmail.com>
2020-08-04 21:33:39 -0300

    Trocando o Logo

--- src/components/Footer/index.js
+++ src/components/Footer/index.js
@@ -4,14 +4,14 @@ import { FooterBase } from './styles';
 function Footer() {
   return (
     <FooterBase>
-      <a href="https://www.alura.com.br/">
-        <img src="https://www.alura.com.br/assets/img/alura-logo-white.1570550707.svg" alt="Logo Alura" />
+      <a href="/">
+        
       </a>
       <p>
         Orgulhosamente criado durante a
         {' '}
-        <a href="https://www.alura.com.br/">
-          Imersão React da Alura
+        <a href="/">
+          Imersão React
         </a>
       </p>
     </FooterBase>
